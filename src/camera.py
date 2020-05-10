6e31d0510e9eb55d766c42678b7eff138731f6ef
HanyuuLu <HanyuuLu@outlook.com>
2020-05-10 16:57:37 +0800

    202005101657

--- src/camera.py
+++ src/camera.py
@@ -1,12 +1,15 @@
 import cv2.cv2 as cv2
 import lpr
 import time
-cap = cv2.VideoCapture(0)
+timeStamp = time.time()
+cap = cv2.VideoCapture(1)
 image = cv2.imread('demo.jpg')
 while True:
     ret, frame = cap.read()
-    # cv2.imshow('capture', frame)
+    cv2.imshow('capture', frame)
     res = lpr.recognition(frame)
-    print(res, end='\r')
+    print("\r%s %f ms             " %
+          (res, (time.time() - timeStamp)*1000), end='')
+    timeStamp = time.time()
     if (cv2.waitKey(1) & 0xff) == ord('s'):
         break
